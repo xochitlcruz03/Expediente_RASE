3ee1cf6d37a061449425ccf92d5c96cc645c85c3
Daniel Hernandez <dani2203g@gmail.com>
2021-10-28 23:59:18 -0500

    Creacion de modelos para la bd y notas para utilizar una BD existente

---
+++ Expediente_RASE/Models/especialidad.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Expediente_RASE.Models
+{
+    /// <summary>
+    /// Daniel Henrnandez 28-10-2021
+    /// </summary>
+    public class especialidad
+    {
+        [Key]
+        public int Id_esp { get; set; }
+
+        [Required]
+        public string Nombre_esp { get; set; }
+    }
+}
