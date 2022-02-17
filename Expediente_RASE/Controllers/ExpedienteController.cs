286f999299df7f3ec1ecf10379d7bedbc3ad101c
xochitlcruz03 <cruzxochitl68@gmail.com>
2022-02-16 23:30:27 -0600

    todo

---
+++ Expediente_RASE/Controllers/ExpedienteController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Expediente_RASE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExpedienteController : ControllerBase
+    {
+        // GET: api/<ExpedienteController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ExpedienteController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ExpedienteController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ExpedienteController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ExpedienteController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
