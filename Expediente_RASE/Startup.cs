b8ebc9e94f5254ef6a9950b23b10f3fb387423e5
xochitlcruz03 <cruzxochitl68@gmail.com>
2022-03-06 18:39:41 -0600

    cors

--- Expediente_RASE/Startup.cs
+++ Expediente_RASE/Startup.cs
@@ -75,15 +75,15 @@ namespace Expediente_RASE
                  });*/
 
             //DANIEL 22/11/2021
-            //CORS
-            /*services.AddCors(options =>
+            //CORS dont move
+            services.AddCors(options =>
             {
                 options.AddPolicy("CorsPolicy",
                     builder => builder.AllowAnyOrigin()
                     .AllowAnyMethod()
                     .AllowAnyHeader()
                     .AllowCredentials());
-            });*/
+            });
 
             //JSON xochitl
             services.AddControllersWithViews()
@@ -104,7 +104,7 @@ namespace Expediente_RASE
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
-            //app.UseCors("CorsPolicy");
+            app.UseCors("CorsPolicy");
 
             if (env.IsDevelopment())
             {
